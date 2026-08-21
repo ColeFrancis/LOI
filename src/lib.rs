29986632aa0f0615aea1a24d61db91c71527525c
Cole <coleshanefrancis@gmail.com>
2026-08-21 16:46:22 -0600

    archiving older portions of the project (pre-compiler) so they can be reimplemented later)

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub mod simulator;
-pub mod network;
-pub mod core;
-pub mod file;
 pub mod compiler;
+pub mod simulator;
--- src/old/simulator/mod.rs
+++ src/old/simulator/mod.rs
@@ -12,4 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-mod interpreter;
+pub mod sim;
+pub mod scheduler;
+pub mod event;
+pub mod watcher;
